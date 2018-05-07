3f8be548fffb0bb9a7f4860576573237053baf1a
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-07 10:39:23 -0700

    installed needed dependencies

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+//connect to mongo
+mongoose
+  .connect('mongodb://localhost:27017/beardb')
+  .then(mongo => {
+    console.log('connected to database');
+  }).catch(err => {
+    console.log('Error connecting to database', err);
+});
 
 const bearController = require('./bears/bearController');
 
