741fc3da5d3f3ab164cb4e5bf8f0a64792ce43d8
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-07 10:50:20 -0700

    made a bear schema/model

---
+++ bears/bearModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const definition = {
+    species: {
+        type: String,
+        required: true
+    },
+    latinName:{
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    },
+};
+
+const options = {
+    timestamps: true,
+};
+
+const bearSchema = new mongoose.Schema(definition, options);
+
+const bearModel = mongoose.model('Bear', bearSchema, 'bears');
+
+module.exports = bearModel;
